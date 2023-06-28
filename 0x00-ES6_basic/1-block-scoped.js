1200f0200746427e094a785a407de805b03e4cb9
joyogbu <obyjoy@yahoo.com>
2023-06-28 14:18:17 +0000

    task 2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,8 +1,8 @@
-const task = false;
-const task2 = true;
+// const task = false;
+// const task2 = true;
 export default function taskBlock(trueOrFalse) {
-  // let task = false;
-  // let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
     const task = true;
