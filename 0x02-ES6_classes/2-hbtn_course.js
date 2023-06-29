6922a3e6fee2685df03d59552a97ef29ff1c7afb
joyogbu <obyjoy@yahoo.com>
2023-06-29 21:58:56 +0000

    task 3

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -4,34 +4,37 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
+
   // getter
   get name() {
-	  return this._name;
+    return this._name;
   }
+
   set name(name) {
-	  if (typeof name != "string") {
-		  throw('TypeError: Name must be a string');
-	  }
-	  else {
-	  	this._name = name;
-	  }
+    if (typeof name != "string") {
+      throw ('TypeError: Name must be a string');
+    } else {
+      this._name = name;
+    }
   }
+
   get length() {
-	  return this._length;
+    return this._length;
   }
+
   set length(length) {
-	  if (typeof length != "number") {
-	  //if (isNaN(length)) {
-	  	throw('TypeError: Length must be a number');
-          }
-	  else { 
-	  	this._length = length;
-	  }
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    } else {
+      this._length = length;
+    }
   }
+
   get students() {
-	  return this._students;
+    return this._students;
   }
+
   set students(students) {
-	  this._students = students;
+    this._students = students;
   }
 }
