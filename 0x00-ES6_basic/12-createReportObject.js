9449f0a740c067afe5e3d5deb1ccb0a0b6206f2f
joyogbu <obyjoy@yahoo.com>
2023-07-01 20:20:11 +0000

    task 12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,38 +1,16 @@
-import createEmployeesObject from './11-createEmployeesObject.js';
+// import createEmployeesObject from './11-createEmployeesObject';
 
-let employeesList = createEmployeesObject;
+// const employeesList = createEmployeesObject;
 export default function createReportObject(employeesList) {
-  //let employeesList = createEmployeesObject;
-  /*const mainDict = {};
-  mainDict[allEmployees] = {};
-  mainDict[allEmployees][[`${Object.keys(employeesList)}`]] = [`${Object.values(employeesList)}`];
-
-  mainDict[`${getNumberOfDepartments()}`] = function() {
-    let count = 0;
-    for (let key in [`${Object.keys(employeesList)}`]) {
-      count += 1;
-    }
-    return count;
-   }
-   return mainDict;
-*/
-   let arr = {};
-   Object.entries(employeesList).forEach(([ke, va]) => {
-		arr[ke] = va;
-   });
-   let mainDict = {
-        allEmployees: arr,
-			
-		//[`${Object.keys(employeesList)}`]: [`${Object.values(employeesList)}`]
-        //},
-        getNumberOfDepartments(employeesList) {
-              let count = 0;
-              for (let key of [`${Object.keys(employeesList)}`]) {
-                   count += 1;
-              }
-            
-            return Object.keys(employeesList).length;
-        }
-    }
-    return mainDict;
+  const arr = {};
+  Object.entries(employeesList).forEach(([ke, va]) => {
+    arr[ke] = va;
+  });
+  const mainDict = {
+    allEmployees: arr,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+  return mainDict;
 }
