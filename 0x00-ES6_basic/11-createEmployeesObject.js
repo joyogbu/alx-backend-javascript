0a3291041d9ce649eb23f38a81c3e277135ed140
joyogbu <obyjoy@yahoo.com>
2023-07-01 09:08:41 +0000

    task 11

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
-  //return `{ ${departmentName}: [ '${[employees.join(', ')]}' ] }`;
- const obj = {}
-  obj[`${departmentName}`] = `[${employees}]`;
-  return obj;
+  return `{ ${departmentName}: [ '${[employees.join("', '")]}' ] }`;
+  // const obj = {}
+  // obj[`${departmentName}`] = `[${employees}]`;
+  // return obj;
 }
--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,16 +2,16 @@ import createEmployeesObject from './11-createEmployeesObject.js';
 
 const employeesList = createEmployeesObject();
 export default function createReportObject(employeesList) {
-  getNumberOfDepartments(employeesList);
-  {
+  const mainDict = {};
+  mainDict.allEmployees = {};
+  mainDict.allEmployees[`${Object.keys(employeesList)}`] = [`${Object.values(employeesList)}`];
+  return mainDict;
+
+  function getNumberOfDepartments(employeesList) {
     let count = 0;
     for (const key in employeesList) {
       count += 1;
     }
     return count;
   }
-  const mainDict = {};
-  mainDict.allEmployees = {};
-  mainDict.allEmployees[`${Object.keys(employeesList)}`] = [`${Object.values(employeesList)}`];
-  return mainDict;
 }
