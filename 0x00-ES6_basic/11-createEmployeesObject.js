b4f55a586809459bc2f1897b3692fc0e1c96b261
joyogbu <obyjoy@yahoo.com>
2023-07-01 10:11:16 +0000

    no 11

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,13 @@
 export default function createEmployeesObject(departmentName, employees) {
-  return `{ ${departmentName}: [ '${[employees.join("', '")]}' ] }`;
+ // return `{ ${departmentName}: [ '${[employees.join("', '")]}' ] }`;
   // const obj = {}
   // obj[`${departmentName}`] = `[${employees}]`;
   // return obj;
+ const obj = {}
+ var arr = [];
+ for (let x of employees) {
+   arr.push(x);
+ }
+  obj[`${departmentName}`] = arr;
+  return obj;
 }
