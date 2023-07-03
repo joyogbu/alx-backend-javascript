712b8e99ad9119a1595cb98d8e544f9ece846633
joyogbu <obyjoy@yahoo.com>
2023-07-03 09:25:41 +0000

    tasi 100

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,26 @@
+export default function createIteratorObject(report) {
+	for (let key of Object.keys(report)) {
+		//if (report.hasOwnProperty(key)) {
+		//return [`${[key]}`];
+		return [`${report[key]}`];
+	}
+	//}*/
+/*	Object.entries(report).forEach(([key,value]) => {
+	console.log(value)
+	});*/
+//	return result;
+	/*report[Symbol.iterator] = function() {
+		let objs = Object.keys(this);
+		let count = 0;
+		isDone = false;
+		let next = () => {
+			if (count>=objs.length)
+			{
+				isDone = true;
+			}
+			return{done: isDone, value: this[objs[count++]]};
+		}
+		return {next};
+	}*/
+	
+}
