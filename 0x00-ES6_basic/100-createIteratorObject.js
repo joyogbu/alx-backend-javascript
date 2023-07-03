b9c11d8619feae53c6b26e3dcc2220307ce75ce1
joyogbu <obyjoy@yahoo.com>
2023-07-03 17:50:46 +0000

    edited 100z'

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,27 +1,28 @@
 export default function createIteratorObject(report) {
-	for (let key of Object.keys(report)) {
-		//if (report.hasOwnProperty(key)) {
-		return [`${[key]}`];
-		//return [`${value}`];
-		//return [`${report[key]}`];
-	}
-	//}*/
-/*	Object.entries(report).forEach(([key,value]) => {
-	console.log(value)
-	});*/
-//	return result;
-	/*report[Symbol.iterator] = function() {
-		let objs = Object.keys(this);
-		let count = 0;
-		isDone = false;
-		let next = () => {
-			if (count>=objs.length)
-			{
-				isDone = true;
-			}
-			return{done: isDone, value: this[objs[count++]]};
-		}
-		return {next};
-	}*/
-	
+  let result;
+  for (const key of Object.keys(report)) {
+    // return [`${[key]}`];
+    // return [`${value}`];
+    result = [`${report[key]}`];
+  }
+  return [`${result}`];
+  /* report[Symbol.iterator] = () => {
+  let objs = Object.keys(this);
+  console.log(obj);
+  let count = 0;
+  let count2 = 0;
+  isDone = false;
+  return {
+    next() {
+      if (count>=objs[count2].length) {
+        count2 = count2+ 1;
+        count = 0;
+      }
+      if (count2>=objs.length) {
+        //isDone = true;
+        return{done: true, value:undefined};
+      }
+      return{done: false, value: this[count++][count2++]};
+    }
+  } */
 }
