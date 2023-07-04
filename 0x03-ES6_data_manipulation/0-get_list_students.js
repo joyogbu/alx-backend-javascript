45f4295e39f075890525063a0e7895d21fd43f3b
joyogbu <obyjoy@yahoo.com>
2023-07-04 10:12:04 +0000

    task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,22 +1,22 @@
 export default function getListStudents() {
-	let myArray = [];
-	let myObject = {
-		 id: 1,
-		 firstName: 'Guillaume', 
-		 location: 'San Francisco'
-	}
-	let myObject2 = {
-		id: 2,
-		firstName: 'James',
-		location: 'Columbia'
-	}
-	let myObject3 = {
-		id: 2,
-		firstName: 'James',
-		location: 'Columbia'
-	}
-	myArray.push(myObject);
-	myArray.push(myObject2);
-	myArray.push(myObject3);
-	return (myArray);
+  const myArray = [];
+  const myObject = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+  const myObject2 = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+  const myObject3 = {
+    id: 2,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+  myArray.push(myObject);
+  myArray.push(myObject2);
+  myArray.push(myObject3);
+  return (myArray);
 }
