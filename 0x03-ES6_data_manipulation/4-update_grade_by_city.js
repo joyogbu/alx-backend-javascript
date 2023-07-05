d0e33fea41fa2d5e40e34d273a8ed3c9f000cc3c
joyogbu <obyjoy@yahoo.com>
2023-07-05 00:55:47 +0000

    edited tas5

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,15 @@
 export default function updateStudentGradeByCity(arg1, arg2, arg3) {
-	let result = arg1.filter((e) => e.location === arg2).map((item) => {
-		//container = {};
-		item.grade = Object.values(arg3).filter((elem) => elem === item.id);
-	
-	});
-	return result;
+  const result = arg1.filter((e) => e.location === arg2).map((item) => {
+    // let container = {};
+    // item.grade = Object.values(arg3).filter((elem) => elem === item.id);
+    let arr = {};
+    /* eslint-disable no-constant-condition */
+    if (typeof arg3.filter((e) => e.grade !== 'undefined' || e.grade !== null)) { /* arg3.map((e) => { e.grade = 'N/A';})} */
+      arr = ({ ...item, grade: arg3.find((elem) => elem.studentId === item.id).grade });
+    } else {
+      arr = ({ ...item, grade: 'N/A' });
+    }
+    return arr;
+  });
+  return result;
 }
