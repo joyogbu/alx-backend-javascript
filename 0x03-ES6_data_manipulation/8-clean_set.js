1ae72f19baeaa31bceea655735da3d26229688f7
joyogbu <obyjoy@yahoo.com>
2023-07-07 09:25:14 +0000

    task 8 edoted

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,14 @@
 export default function cleanSet(set, start) {
   // let newSet = new Set();
   const newSet = [];
+  if (start === '') {
+    return ('');
+  }
   for (const elem of set) {
     if (elem.startsWith(start)) {
       const newElem = elem.replace(start, '');
       newSet.push(newElem);
     }
   }
-  // for (let item of newSet) {
   return (newSet).toString().replace(/,/g, '-');
 }
