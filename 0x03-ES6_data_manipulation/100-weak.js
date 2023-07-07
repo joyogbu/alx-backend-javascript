bcd9e3428c6da8ac192f6fdb4eca30ae38d0fa62
joyogbu <obyjoy@yahoo.com>
2023-07-07 16:49:53 +0000

    task 100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,10 @@
 export const weakMap = new WeakMap();
-export default function queryAPI(endpoint) {
-  let count = 0;
-  weakMap.set(endpoint, count);
-  count += 1;
+export function queryAPI(endpoint) {
+  const count = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, count + 1);
+  // count += 1;
   if (count >= 5) {
     throw new Error('Endpoint load is high');
   }
-  return (weakMap);
+  return (count);
 }
