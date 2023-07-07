06fe52c09275395d0dd094ad127cbb7cfbce2b86
joyogbu <obyjoy@yahoo.com>
2023-07-07 12:26:27 +0000

    task 100 edited

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,7 @@
 export default function updateUniqueItems(map) {
-  /* if (map instanceof Map === 'False') {
-    return `Cannot process`;
-  } */
+  if (map instanceof Map === 'False') {
+    throw new Error('Cannot process');
+  }
   map.forEach((value, key) => {
     /* if map instanceof Map === 'False' {
       return */
