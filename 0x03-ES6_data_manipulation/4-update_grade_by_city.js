32079df28f4e822984b65fd449420443fcedb15d
joyogbu <obyjoy@yahoo.com>
2023-07-08 02:02:42 +0000

    corrected task no 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,4 @@
 export default function updateStudentGradeByCity(arg1, arg2, arg3) {
-  const result = arg1.filter((e) => e.location === arg2).map((item) => {
-    // let container = {};
-    // item.grade = Object.values(arg3).filter((elem) => elem === item.id);
-    let arr = {};
-    /* eslint-disable no-constant-condition */
-    if (typeof arg3.filter((e) => e.grade !== 'undefined' || e.grade !== null)) { /* arg3.map((e) => { e.grade = 'N/A';})} */
-      arr = ({ ...item, grade: arg3.find((elem) => elem.studentId === item.id).grade });
-    } else {
-      arr = ({ ...item, grade: 'N/A' });
-    }
-    return arr;
-  });
+  const result = arg1.filter((e) => e.location === arg2).map((item) => ({ ...item, grade: (arg3.find((elem) => elem.studentId === item.id) || { grade: 'N/A' }).grade }));
   return result;
 }
