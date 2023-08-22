a0a71b5991903e22495295fcf9004e5360da317a
joyogbu <obyjoy@yahoo.com>
2023-08-22 09:00:12 +0000

    task

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,1 +1,9 @@
-
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', data => {
+	console.log(`Your name is: ${data}`);
+});
+process.on('SIGINT', () => {
+	console.log('This important software is now closing');
+});
+/*process.exit(0);
+console.log('Ok');*/
