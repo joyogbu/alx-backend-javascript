5c6b4c5dc9e7b2a513a43b4fdf736966062a2c45
joyogbu <obyjoy@yahoo.com>
2023-08-22 09:27:16 +0000

    task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
-  process.stdout.write('Your name is: ' + data);
+  process.stdout.write(`Your name is: ${data}`);
 });
-process.stdin.on('end', function() {
-console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
