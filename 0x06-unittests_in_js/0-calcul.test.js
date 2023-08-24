afe05e462e5527f53cb2b795f6eb3339b39b795b
joyogbu <obyjoy@yahoo.com>
2023-08-24 23:46:16 +0000

    task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,26 @@
+const assert = require('assert')
 const calculateNumber = require("./0-calcul.js");
-var assert = require('chai').assert;
-assert.equal(calculateNumber(1, 3), 4)
-assert.equal(calculateNumber(1, 3.7), 5)
-assert.equal(calculateNumber(1.2, 3.7), 5)
+describe('calculateNumber', function () {
+	it("compare sumz", function() {
+		//const calculateNumber = require("./0-calcul.js");
+		assert.equal(calculateNumber(1, 3), 4)
+	});
+	it("compare with float", function () {
+		assert.equal(calculateNumber(1, 3.7), 5)
+	});
+	it("compare another float", function () {
+		assert.equal(calculateNumber(1.2, 3.4), 5)
+	});
+	it("compare another float", function () {
+                assert.equal(calculateNumber(2.5, 3.2), 6)
+        });
+	it("compare negative float", function () {
+                assert.equal(calculateNumber(1.2, -3.0), -2)
+        });
+	it("compare negative integer", function (){ 
+                assert.equal(calculateNumber(-3.6, 7), 3)
+        });
+	it("round down negative float", function () {
+                assert.equal(calculateNumber(-3.5, 7), 4)
+        });
+});
