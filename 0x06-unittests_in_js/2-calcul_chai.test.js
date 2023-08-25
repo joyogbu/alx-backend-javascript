7be56bb147464a13c6d0fe9139af9d24c263f631
joyogbu <obyjoy@yahoo.com>
2023-08-25 15:38:02 +0000

    task 2

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const expect = require('chai').expect;
-const calculateNumber = require("./1-calcul.js");
+const calculateNumber = require("./2-calcul_chai.js");
 describe('calculateNumber', function () {
 	it("compare sumz", function() {
 		//const calculateNumber = require("./0-calcul.js");
