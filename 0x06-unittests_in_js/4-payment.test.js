1e1397d5916810a471584eec35593fa288be5338
joyogbu <obyjoy@yahoo.com>
2023-08-26 01:06:15 +0000

    task 3

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,27 @@
+const expect = require('chai').expect
+const sinon = require('sinon')
+const Utils = require("./utils.js");
+const sendPaymentRequestToApi = require('./3-payment.js');
+describe('sendPaymentRequestToApi', function () {
+	let calledFuncSpy;
+	let calledConsole;
+	beforeEach(function() {
+		calledFuncSpy = sinon.spy(Utils);
+		calledConsole = sinon.spy(console, 'log')
+	});
+	/*afterEach(function() {
+		calledFuncSpy.restore();
+	});*/
+	it('test the sum function', function() {
+		expect(sendPaymentRequestToApi(100, 20)).to.equal(console.log('The total is: 120'));
+		//expect(calledConsole.calledOnce).to.be.true;
+		//expect(calledConsole.calledWith('The total is: 120')).to.be.true;
+	});
+	it('validate utils function', function() {
+		expect(Utils.calculateNumber('SUM', 100, 20)).to.equal(120);
+	});
+	afterEach(function() {
+		calledFuncSpy.calculateNumber.restore();
+		calledConsole.restore();
+        });
+});
