9a52da50b66b43990dbec99cdf5d8a153b972d76
joyogbu <obyjoy@yahoo.com>
2023-08-26 22:28:04 +0000

    task 8

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 7865;
-const server = app.listen(port);
-console.log(`API available on localhost port ${port}`)
+app.listen(port, () => {
+	console.log(`API available on localhost port ${port}`);
+});
 app.get('/', (req, res) => res.send('Welcome to the payment system'));
-module.exports = server
+module.exports = app
