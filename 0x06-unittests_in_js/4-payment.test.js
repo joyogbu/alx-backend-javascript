f0b731465624866c038d1f6d0c3dc89893eefab6
joyogbu <obyjoy@yahoo.com>
2023-08-26 01:40:20 +0000

    task 3

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -6,22 +6,23 @@ describe('sendPaymentRequestToApi', function () {
 	let calledFuncSpy;
 	let calledConsole;
 	beforeEach(function() {
-		calledFuncSpy = sinon.spy(Utils);
+		calledFuncSpy = sinon.stub(Utils, 'calculateNumber').returns (10);
+
 		calledConsole = sinon.spy(console, 'log')
 	});
 	/*afterEach(function() {
 		calledFuncSpy.restore();
 	});*/
 	it('test the sum function', function() {
-		expect(sendPaymentRequestToApi(100, 20)).to.equal(console.log('The total is: 120'));
+		expect(sendPaymentRequestToApi(100, 20)).to.equal(console.log('The total is: 10'));
 		//expect(calledConsole.calledOnce).to.be.true;
-		//expect(calledConsole.calledWith('The total is: 120')).to.be.true;
+		expect(calledConsole.calledWith('The total is: 10')).to.be.true;
 	});
 	it('validate utils function', function() {
-		expect(Utils.calculateNumber('SUM', 100, 20)).to.equal(120);
+		expect(Utils.calculateNumber('SUM', 100, 20)).to.equal(10);
 	});
 	afterEach(function() {
-		calledFuncSpy.calculateNumber.restore();
+		calledFuncSpy.restore();
 		calledConsole.restore();
         });
 });
