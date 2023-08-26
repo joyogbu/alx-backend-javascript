1d7a1593509aa834e52c17e2f5ca5de79df49fce
joyogbu <obyjoy@yahoo.com>
2023-08-26 21:28:35 +0000

    task 8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -5,16 +5,17 @@ const request = require('request')
 const server = require('./api')
 describe('Index Page', function () {
 	it('Correct status code?', function (done) {
-		request.get('http://localhost:7865', function (err, response, body) {
+		request.get('http://localhost:7865/', function (err, response, body) {
 			assert.equal(response.statusCode, 200)
 			done();
 		});
 	});
 	it('Correct result?', function (done) {
-		request.get('http://localhost:7865', function (err, response, body) {
+		request.get('http://localhost:7865/', function (err, response, body) {
 			assert.equal(response.body, 'Welcome to the payment system')
 		done()
 		});
 		//done()
 	});
+	//request.close()
 });
