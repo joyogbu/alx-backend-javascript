7f3d4ccdb0abea506080ad02fb8a5590fbaa33fe
joyogbu <obyjoy@yahoo.com>
2023-09-16 05:33:48 -0700

    task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,15 +1,40 @@
 const countStudents = function(path) {
-const csv = require('csv-parser');
+//const csv = require('csv-parser');
+//const readLine = require('readLine');
 const fs = require('fs');
 const result = [];
+let output;
 if (fs.existsSync(path)) {
 try {
-fs.createReadStream(path)
-  .pipe(csv())
-  .on('data', (data) => result.push(data))
+fs.createReadStream(path, { encoding: 'utf-8' })
+  //.pipe(csv())
+  //.on('data', (data) => result.push(data))
+  .on('data', (data) => {
+	  const lines = data.split('\n');
+	  const mylines = lines.filter(function(elem) {
+		 return elem.trim() !== ''
+	  })
+	  const header = mylines[0].split(',');
+	  //console.log(header)
+	  let rows
+	  let out = []
+	  for (let i = 1; i<=mylines.length-1; i=i+1) {
+		  rows = mylines[i].split(',');
+		  //console.log(rows)
+		  out.push(rows)
+	  }
+	  //console.log(out)
+	  output = out.map((item) => {
+		  return item.reduce((obj, val, index) => {
+			  obj[header[index]] = val;
+			  return obj
+		  }, {});
+	  })
+	  //console.log(output)
+  })
   .on('end', () => {
 	  //console.log(result)
-    console.log(`Number of students: ${result.length}`)
+    console.log(`Number of students: ${output.length}`)
 	  //let elem ={}
 	  /*const item = result.reduce((item, y) => {
 		  return item[y.field] = result.filter((res) => res.field === y.field)
@@ -28,7 +53,7 @@ fs.createReadStream(path)
 	  //console.log(result)
 
 
-	const elements = result.reduce((elements, x) => {
+	const elements = output.reduce((elements, x) => {
 		const field = (elements[x.field] || [])
 		field.push(x)
 		elements[x.field] = field
