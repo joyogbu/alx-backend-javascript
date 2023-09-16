8ed2651770c7351e9b88fc7b68b9a66239db46bf
joyogbu <obyjoy@yahoo.com>
2023-09-16 07:23:59 -0700

    task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,62 +1,54 @@
-const countStudents = function(path) {
-//const csv = require('csv-parser');
-//const readLine = require('readLine');
+/* eslint-disable no-param-reassign */
 const fs = require('fs');
-const result = [];
-let output;
-if (fs.existsSync(path)) {
-try {
-//fs.createReadStream(path, { encoding: 'utf-8' })
-  //.pipe(csv())
-  //.on('data', (data) => result.push(data))
-  //.on('data', (data) => {
-	const data = fs.readFileSync(path, 'utf8')
-	  const lines = data.split('\n');
-	  const mylines = lines.filter(function(elem) {
-		 return elem.trim() !== ''
-	  })
-	  const header = mylines[0].split(',');
-	  //console.log(header)
-	  let rows
-	  let out = []
-	  for (let i = 1; i<=mylines.length-1; i=i+1) {
-		  rows = mylines[i].split(',');
-		  //console.log(rows)
-		  out.push(rows)
-	  }
-	  //console.log(out)
-	  output = out.map((item) => {
-		  return item.reduce((obj, val, index) => {
-			  obj[header[index]] = val;
-			  return obj
-		  }, {});
-	  })
-	  //console.log(output)
- // })
-  //.on('end', () => {
-	  //console.log(result)
-    console.log(`Number of students: ${output.length}`)
-	const elements = output.reduce((elements, x) => {
-		const field = (elements[x.field] || [])
-		field.push(x)
-		elements[x.field] = field
-		return elements
-	}, {});
-	  //console.log(elements)
-	 for (const i in elements) {
-	 const fnames=elements[i].map(it => it.firstname)
-    const res = `${fnames.join(', ')}`
-	console.log(`Number of students in ${i}: ${elements[i].length}. List: ${res}`);
-		 //console.log(`List: ${fnames.join(', ')}`)
-	 }
 
-  //});
-	
-} catch(error) {
-	console.log(error)
-  }
-} else {
-	throw new Error('Cannot load the database')
+const countStudents = (path) => {
+  let output;
+  if (fs.existsSync(path)) {
+    try {
+      // fs.createReadStream(path, { encoding: 'utf-8' })
+      // .on('data', (data) => {
+      const data = fs.readFileSync(path, 'utf8');
+      const lines = data.split('\n');
+      const mylines = lines.filter((elem) => elem.trim() !== '');
+      const header = mylines[0].split(',');
+      let rows;
+      const out = [];
+      for (let i = 1; i <= mylines.length - 1; i += 1) {
+        rows = mylines[i].split(',');
+        // console.log(rows)
+        out.push(rows);
+      }
+      // console.log(out)
+      output = out.map((item) => item.reduce((obj, val, index) => {
+        obj[header[index]] = val;
+        return obj;
+      }, {}));
+      // console.log(output)
+      // })
+      // .on('end', () => {
+      // console.log(result)
+      console.log(`Number of students: ${output.length}`);
+      const elements = output.reduce((elements, x) => {
+        const field = (elements[x.field] || []);
+        field.push(x);
+        elements[x.field] = field;
+        return elements;
+      }, {});
+      // console.log(elements)
+      for (const i in elements) {
+        if (elements.hasOwnProperty.call(elements, i)) {
+          const fnames = elements[i].map((it) => it.firstname);
+          const res = `${fnames.join(', ')}`;
+          console.log(`Number of students in ${i}: ${elements[i].length}. List: ${res}`);
+        // console.log(`List: ${fnames.join(', ')}`)
+        }
+      }
+      // });
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    throw new Error('Cannot load the database');
   }
 };
 module.exports = countStudents;
