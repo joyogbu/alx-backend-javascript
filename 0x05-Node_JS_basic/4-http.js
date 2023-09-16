89db7dd558f7fd6330eb306a263a5ef818cd756d
joyogbu <obyjoy@yahoo.com>
2023-09-16 11:17:29 -0700

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,11 @@
 const http = require('http');
-const host = 'localhost'
-const port = 1245
+
+const host = 'localhost';
+const port = 1245;
 const app = http.createServer((req, res) => {
-	res.end("Hello Holberton School!")});
-app.listen(port, host, () => {
-	console.log('server running')
+  res.end('Hello Holberton School!');
 });
-module.exports = app
+app.listen(port, host, () => {
+  console.log('server running');
+});
+module.exports = app;
